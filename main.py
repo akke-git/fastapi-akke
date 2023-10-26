985a4bf2e9567cdab7ad5104e1e69aa99b123aa8
akke <akkeii@gmail.com>
2023-10-26 16:42:44 +0900

    server ip change3

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ origins = [
     "http://localhost:5173",    # 또는 "http://127.0.0.1:5173"
     "http://localhost:8000"
     "http://127.0.0.1:8000"
+    "http://13.125.234.195:8000"
 ]
 
 app.add_middleware(
