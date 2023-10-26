cbb446e72dc79618403ba622e5c65473445cb020
akke <akkeii@gmail.com>
2023-10-26 16:27:31 +0900

    server ip change

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ app = FastAPI()
 # ---------------------------------------------------------------------------------------------------
 # Origin은 Protocol, Domain, Port 묶음을 이르는 말이다. (Path나 Query String 등은 해당하지 않는다.)
 # CORS란 HTTP 헤더를 통해 한 Origin에서 실행 중인 웹 어플리케이션이 다른 Origin의 리소스에 접근할 수 있도록 브라우저에 권한을 부여하는 정책이다
-# origin을 등록하고 미들웨어를 등록한다 (origin은 htts나 특정포트를 지정하는 등의 경우 등록필요)
+# origin을 등록하고 미들웨어를 등록한다 (origin은 https나 특정포트를 지정하는 등의 경우 등록필요)
 
 origins = [
     "http://localhost:5173",    # 또는 "http://127.0.0.1:5173"
+    "http://localhost:8000"
+    "http://127.0.0.1:8000"
 ]
 
 app.add_middleware(
